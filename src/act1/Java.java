844e0d1f56cf154bcd880d299e89904d07fd8497
cerezasahara <cerezasaharalouisse001@gmail.com>
2026-03-04 11:03:57 +0800

    initial statement

---
+++ src/act1/Java.java
@@ -0,0 +1,14 @@
+package act1;
+
+
+
+public class Java {
+
+    public static void main(String[] args) {
+
+        LoginForm Login = new LoginForm();
+        Welcome welcome = new Welcome();
+        
+        Login.show();
+    }
+}
